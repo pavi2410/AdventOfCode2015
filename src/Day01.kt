a75f56f4970c07d321ceb6f666cf915ed13e6b47
Pavitra Golchha <hello@pavi2410.me>
2021-12-02 12:51:02 +0530

    Day 1: Complete 😁

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input.map { if (it == '(') 1 else -1 }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        var s = 0
+        var p = 0
+        input.map { if (it == '(') 1 else -1 }.dropWhile {
+            s += it
+            p++
+            s != -1
+        }
+        return p
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput: Map<String, Int> = mapOf(
+        "(())" to 0,
+        "()()" to 0,
+        "(((" to 3,
+        "(()(()(" to 3,
+        "))(((((" to 3,
+        "())" to -1,
+        "))(" to -1,
+        ")))" to -3,
+        ")())())" to -3
+    )
 
-    val input = readInput("Day01")
+    for ((key, value) in testInput) {
+        check(part1(key) == value)
+    }
+
+    val input = readInput("Day01")[0]
     println(part1(input))
     println(part2(input))
 }
